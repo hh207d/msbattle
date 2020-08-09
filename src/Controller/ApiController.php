b69c13a5a3d722e1ad337f0e40241a45edf7c685
Marco Surrey <surrey@etracker.com>
2020-08-09 15:12:53 +0200

    wip.. authentication and stuff

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,18 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class ApiController extends AbstractController
 {
-    /**
-     * @Route("/api", name="api")
-     */
-    public function index()
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
     {
-        return $this->render('api/index.html.twig', [
-            'controller_name' => 'ApiController',
-        ]);
+        $em = $this->getDoctrine()->getManager();
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+
+        $user = new User($email);
+        $user->setPassword($encoder->encodePassword($user, $password));
+        $user->setEmail($email);
+
+        $em->persist($user);
+        $em->flush();
+
+        $message = sprintf('User %s successfully created', $user->getUsername());
+        return $this->json($message);
     }
+
+    public function api()
+    {
+        $message = sprintf('Logged in as %s', $this->getUser()->getUsername());
+        return $this->json($message);
+    }
+
 }
