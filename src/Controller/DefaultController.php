99f8662382de016d64f7c2ede823ac929791f8a9
Marco Surrey <surrey@etracker.com>
2020-08-13 00:37:37 +0200

    wip...

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Game;
 use App\Entity\Placement;
 use App\Entity\Ship;
+use App\Entity\Turn;
 use App\Helper\GameState;
 use App\Helper\ShipState;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,14 +24,14 @@ class DefaultController extends AbstractController
         $games =$this->getDoctrine()->getRepository(Game::class)->findAll();
 
         $playerPlacements = [];
-        $opponentPlacements = [];
+        $turns = [];
         $gameState = '';
         $placeableShips = [];
         if($gameid)
         {
             $game = $this->getDoctrine()->getRepository(Game::class)->find($gameid);
             $user = $game->getUser();
-            $placements = $this->getDoctrine()->getRepository(Placement::class)->findBy(
+            $playerPlacements = $this->getDoctrine()->getRepository(Placement::class)->findBy(
                 ['game' => $game, 'user' => $game->getUser()]
             );
 
@@ -42,16 +43,25 @@ class DefaultController extends AbstractController
                     ['game' => $game, 'state' => ShipState::STATE_DOCKED, 'user' => $user]
                 );
             }
+            if($gameState == GameState::STATE_BATTLE)
+            {
+                /** @var Turn[] $turns */
+                $turns = $this->getDoctrine()->getRepository(Turn::class)->findBy(['game' => $game]);
+
+            }
+
         }
 
-        $statisticsResult = [];
+        $amountOfGames = 0;
+        $startedGames = 0;
+        $battlingGames = 0;
+        $finishedGames = 0;
 
         if($statistics)
         {
-            $amountOfGames = 0;
-            $startedGames = 0;
-            $battlingGames = 0;
-            $finishedGames = 0;
+
+
+            $playedGames = [];
 
             foreach ($games as $game)
             {
@@ -67,10 +77,9 @@ class DefaultController extends AbstractController
                     case GameState::STATE_FINISHED:
                         $finishedGames +=1;
                 }
-
-
+                $player = $game->getUser();
+                // TODO: statistics for wins, played, ...
             }
-
 
         }
 
@@ -80,10 +89,14 @@ class DefaultController extends AbstractController
             'games' => $games,
             'gameid' => $gameid,
             'playerPlacements' => $playerPlacements,
-            'opponentplacements' => $opponentPlacements,
-            'statistics' => $statisticsResult,
+            'amountOfGames' => $amountOfGames,
+            'startedGames' => $startedGames,
+            'battlingGames' => $battlingGames,
+            'finishedGames' => $finishedGames,
             'gameState' => $gameState,
             'placeableShips' => $placeableShips,
+            'turns' => $turns,
+
         ]);
     }
 }
